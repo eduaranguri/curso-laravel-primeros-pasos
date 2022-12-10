889015a8b8b5dc28769f5571e6059ca1529210aa
eduaranguri <caremperador@gmail.com>
2022-12-10 09:24:39 -0500

    agregue rutas con nombre

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('inicio');
+})->name('inicio');
+
+Route::get('/custom', function () {
+    $msj2 = "Mensaje desde el servidor *-*";
+    return view('custom',['msj' => $msj2]);
 });
+Route::get('/contacto', function () {
+    return view('contacto');
+})->name('contacto');
+
+
